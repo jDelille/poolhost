a6b2867620dadc9deb41fc8b2764d57594c5df8f
Justin Delille <justind7513@gmail.com>
2021-12-18 00:14:58 -0700

    final

--- server/server.js
+++ server/server.js
@@ -8,14 +8,18 @@ const app = express()
 // nfl news api:
 // 'https://api.nflpickwatch.com/v1/nfl/news'
 
-app.use(express.static(__dirname + '/client'));
+app.use(express.static(path.join(__dirname, '../client/build')))
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/client/dist')))
-    app.get('*', (req,res) => {
-      res.sendFile(path.join(__dirname, '/client/dist', 'index.html'))
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) // relative path
     })
   }
+
+
 
 
 //ESPN API
