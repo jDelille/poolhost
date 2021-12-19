44dc6ac133c94ef22211ca0738dbab304d0ad20a
Justin Delille <justind7513@gmail.com>
2021-12-19 12:41:25 -0700

    backend

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/public'))
+    app.use(express.static('client/build'))
 }
 
 
