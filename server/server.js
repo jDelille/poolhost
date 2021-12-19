ca34f5fcb45e0317aafc3a325ccfae7d5df88b71
Justin Delille <justind7513@gmail.com>
2021-12-19 12:27:54 -0700

    backend

--- server/server.js
+++ server/server.js
@@ -13,7 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
-
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+}
 
 
 
@@ -39,10 +42,7 @@ app.get("/expert", async (req, res) => {
 })
 
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-}
+
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
         if(err) {
