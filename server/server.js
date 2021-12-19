b93bbce4843e76ee22c51f862c91056e98e46431
Justin Delille <justind7513@gmail.com>
2021-12-19 12:21:39 -0700

    backend

--- server/server.js
+++ server/server.js
@@ -43,8 +43,12 @@ app.get("/expert", async (req, res) => {
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
 }
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
+        if(err) {
+            res.status(500).send(err)
+        }
+    });
   });
   
 
