bc89b47703f087f2181d14fb278c46f58c0ee3a0
Justin Delille <justind7513@gmail.com>
2021-12-30 19:43:02 -0700

    static.json

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import {HashRouter} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-    
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
