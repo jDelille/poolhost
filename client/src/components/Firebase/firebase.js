b4c68485578445a129d658b936714d41537abd87
Justin Delille <justind7513@gmail.com>
2022-01-02 12:26:39 -0700

    added .env

--- client/src/components/Firebase/firebase.js
+++ client/src/components/Firebase/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth"
 import 'firebase/firestore'
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyCJE0YDdSWqqp2W_dgSgpSslPfJZyZQrOQ",
-  authDomain: "sockhost-3a550.firebaseapp.com",
-  databaseURL: "https://sockhost-3a550-default-rtdb.firebaseio.com",
-  projectId: "sockhost-3a550",
-  storageBucket: "sockhost-3a550.appspot.com",
-  messagingSenderId: "77594944906",
-  appId: "1:77594944906:web:d71872fb879e23257c9473"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 })
 
 export const firebaseRef = firebase;
