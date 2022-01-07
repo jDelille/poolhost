f2c182827adcf0fd9d0206555aa805c9db27b08e
Justin Delille <justind7513@gmail.com>
2022-01-06 22:37:40 -0700

    cleaned up code

--- server/server.js
+++ server/server.js
@@ -1,54 +1,49 @@
-const express = require('express')
-const axios = require('axios')
-const path = require('path')
+const express = require("express");
+const axios = require("axios");
+const path = require("path");
 
-const app = express()
-
+const app = express();
 
 // nfl news api:
 // 'https://api.nflpickwatch.com/v1/nfl/news'
 
-
 app.use(express.urlencoded({ extended: true }));
 
-
-
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
-
-
-
 
 //ESPN API
 
 app.get("/gamebar", async (req, res) => {
-    const response = await axios.get('https://site.web.api.espn.com/apis/v2/scoreboard/header?sport=football&league=nfl')
-    const news = response.data;
-    res.status(200).send(news)
-})
+  const response = await axios.get(
+    "https://site.web.api.espn.com/apis/v2/scoreboard/header?sport=football&league=nfl"
+  );
+  const news = response.data;
+  res.status(200).send(news);
+});
 
 app.get("/news", async (req, res) => {
-    const response = await axios.get('https://site.api.espn.com/apis/site/v2/sports/football/nfl/news')
-    const news = response.data;
-    res.status(200).send(news)
-})
+  const response = await axios.get(
+    "https://site.api.espn.com/apis/site/v2/sports/football/nfl/news"
+  );
+  const news = response.data;
+  res.status(200).send(news);
+});
 
 app.get("/expert", async (req, res) => {
-    const response = await axios.get('https://api.nflpickwatch.com/v1/nfl/picks/2021/11/su/experts/true/25/0')
-    const news = response.data;
-    res.status(200).send(news)
-})
+  const response = await axios.get(
+    "https://api.nflpickwatch.com/v1/nfl/picks/2021/11/su/experts/true/25/0"
+  );
+  const news = response.data;
+  res.status(200).send(news);
+});
 
-
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-})
-
-  
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => console.log('Avengers... Assemble!'));
+app.listen(PORT, () => console.log("Avengers... Assemble!"));
